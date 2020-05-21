1e316582950a1b29a1f6b1af69ef834fb4be59d7
Jesus Ramirez <jesusdevelp@gmail.com>
2020-05-21 12:48:20 -0500

    Building basic components

--- src/App.js
+++ src/App.js
@@ -2,9 +2,22 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import ProductList from "./components/ProductList";
+import Details from "./components/Details";
+import Cart from "./components/Cart";
+import Default from "./components/Default";
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <React.Fragment>
+      <Navbar />
+      <ProductList />
+      <Details />
+      <Cart />
+      <Default />
+    </React.Fragment>
+  );
 }
 
 export default App;
