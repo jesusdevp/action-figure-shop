bd2f9d3cf1c701790b67c9bc4b157697bd112a9e
Jesus Ramirez <jesusdevelp@gmail.com>
2020-05-22 14:01:49 -0500

    Added footer card and transition to product image

--- src/components/Product.js
+++ src/components/Product.js
@@ -16,6 +16,7 @@ export default class Product extends Component {
             <Link to="/details">
               <img src={img} alt="product" className="card-img-top" />
             </Link>
+            {/* Add to Card Button */}
             <button
               className="cart-btn"
               disabled={inCart ? true : false}
@@ -33,10 +34,47 @@ export default class Product extends Component {
               )}
             </button>
           </div>
+          {/* Card footer */}
+          <div className="card-footer d-flex justify-content-between">
+            <p className="align-self-center mb-0">{title}</p>
+            <h5 className="text-blue font-italic mb-0">
+              <span className="mr-1">$</span>
+              {price}
+            </h5>
+          </div>
         </div>
       </ProductWrapper>
     );
   }
 }
 
-const ProductWrapper = styled.div``;
+const ProductWrapper = styled.div`
+  .card {
+    border-color: transparent;
+    transition: all 0.85s linear;
+  }
+  .card-footer {
+    background: transparent;
+    border-top: transparent;
+    transition: all 0.85s linear;
+  }
+  &:hover {
+    .card {
+      border: 0.04rem solid rgba(0, 0, 0, 0.2);
+      box-shadow: 2px 2px 5px 0px rgba(0, 0, 0, 0.2);
+    }
+    .card-footer {
+      background: rgba(247, 247, 247);
+    }
+  }
+  .img-container {
+    position: relative;
+    overflow: hiden;
+  }
+  .card-img-top {
+    transition: all 0.85s linear;
+  }
+  .img-container:hover .card-img-top {
+    transform: scale(1.2);
+  }
+`;
